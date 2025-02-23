80e9bdac7991112c4fa927101efa1e67434d3f2e
Zaur-Ismailov <ismailovzt@gmail.com>
2025-02-23 15:35:31 +0300

    Добавлен static

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'), # views.py should contain def index(request)..... 
     path('services/', views.services, name='services'), 
-
+# добавить пути к остальным страницам
 ]
